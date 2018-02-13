35a8d7d9e6c102c075f49bf5f360d4d0c132ca2a
David Ornellast <ornellasd@gmail.com>
2018-02-12 20:44:01 -0800

    first unit test and url mapping, dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.broser.implicitly_wait(3)
+        self.browser.implicitly_wait(3)
         
     def tearDown(self):
         self.browser.quit()
