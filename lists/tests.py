9ac3d6ec09e3c283d1e284885a8baf41e465df97
David Ornellas <ornellasd@gmail.com>
2018-02-13 11:09:30 -0800

    refactors home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -14,11 +14,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do-lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-#class SmokeTest(TestCase):
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
 
-#    def test_bad_maths(self):
-#        self.assertEqual(1 + 1, 3)
+       
