3d1580aa52883e48b3b4e40a38492af2037f509e
David Ornellast <ornellasd@gmail.com>
2018-02-12 21:34:22 -0800

    functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,22 +1,6 @@
 from selenium import webdriver
-
-## User stories ##
-
-# he is invited to enter a to-do
-
-# he types "Buy bones" in text box
-
-# he hits enter and now "Buy bones" appears as to-do item
-
-# text box remains, Scooter enters "sleep and poop"
-
-# page updates, both list items are shown
-
-# scooter takes not of url to save his list
-
-# Scooter derps
-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -32,9 +16,40 @@ class NewVisitorTest(unittest.TestCase):
         # Scooter goes to checkout the homepage
         self.browser.get('http://localhost:8000')
 
-        # He notices tile mentions to-do lists
+        # He notices page title mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # he is invited to enter a to-do
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # he types "Buy bones" in text box
+        inputbox.send_keys('Buy peacock feathers')
+        
+        # he hits enter and now "Buy bones" appears as to-do item
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # text box remains, Scooter enters "sleep and poop"
         self.fail('Finish the test!')
 
+        ## User stories ##
+        # page updates, both list items are shown
+        
+        # scooter takes not of url to save his list
+        
+        # Scooter derps
+        
 if __name__ == '__main__':
     unittest.main(warnings='ignore') 
