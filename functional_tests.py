0e6715d20819544e25a758f86d1cdc4262f7a1d9
David Ornellas <ornellasd@gmail.com>
2018-02-13 11:24:12 -0800

    front page html now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # he types "Buy bones" in text box
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy some chew bones')
         
         # he hits enter and now "Buy bones" appears as to-do item
         inputbox.send_keys(Keys.ENTER)
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy some chew bones' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # text box remains, Scooter enters "sleep and poop"
