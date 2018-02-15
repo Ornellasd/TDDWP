b4c2ad43f07c45516bdf1ee5d99ffc344330a7a7
David Ornellas <ornellasd@gmail.com>
2018-02-15 11:36:13 -0800

    makes functional_tests an app, uses LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from selenium import webdriver
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium import webdriver
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -19,7 +19,8 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Scooter goes to checkout the homepage
-        self.browser.get('http://localhost:8000')
+        #self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices page title mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -54,11 +55,4 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
         
         ## User stories ##
-        
-        
-        
-        
         # Scooter derps
-        
-if __name__ == '__main__':
-    unittest.main(warnings='ignore') 
