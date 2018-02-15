b6dc3f2f5ff9f7938dd3dbdb0e70c8d6e2f06111
David Ornellas <ornellasd@gmail.com>
2018-02-15 12:03:36 -0800

    initalizes FT to account for separate lists by different users(separate urls)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,6 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
         
         # he hits enter and now "Buy bones" appears as to-do item
         inputbox.send_keys(Keys.ENTER)
+        scooter_list_url = self.browser.current_url
+        self.assertRegex(scooter_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy some chew bones')
                 
         # text box remains, Scooter enters "Chew bone and and sleep"
@@ -50,9 +52,41 @@ class NewVisitorTest(LiveServerTestCase):
         # page updates, both list items are shown
         self.check_for_row_in_list_table('1: Buy some chew bones')
         self.check_for_row_in_list_table('2: Chew bone and sleep')
-        
+            
+        # now a new user, Spike, comes along to the site
+        ## we use a new browser session to make sure that no information
+        ## of scooter's list is coming thourgh from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # Spike visits the home page, no sign of Scooter's list apparent
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy some chew bones', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Spike starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy pig ear')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Spike gets his own unique URL
+        spike_list_url = self.browser.current_url
+        self.assertRegex(spike_list_url, '/lists/.+')
+        self.assertNotEqual(spike_list_url, scooter_list_url)
+
+        # Again, no trace of scooter's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy some chew bones', page_text)
+        self.assertIn('Buy pig ear', page_text)
+
+        # satisfied, they both blep
+
+
+
+
         # scooter takes note of url to save his list
-        self.fail('Finish the test!')
-        
-        ## User stories ##
-        # Scooter derps
+        #self.fail('Finish the test!')
+
+        # scooter visits the url, his to-do list is still there
+        # satisfied, he chews his bone on his bed
